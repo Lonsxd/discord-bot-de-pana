4393c32603ff4eaf341d68fe42092dc0bd24cf12
losnx <leoprovlogs4@gmail.com>
2021-07-08 15:12:31 -0400

    xd
    xd

--- commands/hello.js
+++ commands/hello.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'hello',
     description: "this is a hello command!",
     execute(message, args){
-        message.channel.send('hello whats up bro');
+        message.channel.send('hello whats up bro 🤑');
     }
 }
